ead809a8637bf0726fe7db42ebab044908126209
Aung Khant <theaungkhant@gmail.com>
2016-11-06 23:54:18 +0630

    Fixing Search Form for vue js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,14 +13,14 @@
 
                 <div class="alert alert-danger" role="alert" v-if="error">
                     <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                    @{{ noResult }}
+                    @{{ error }}
                 </div>
 
                 <div class="form-group">
 
                     <div class="input-group input-group-md">
                         <div class="icon-addon addon-md">
-                            <input type="text" placeholder="Search here!" v-mode"query" class="form-control">
+                            <input type="text" placeholder="Search here!" v-model="query" class="form-control">
                         </div>
 
                         <span class="input-group-btn">
@@ -31,10 +31,27 @@
                     </div>
                 </div>
             </div>
+
             <div id="products" class="row list-group">
+                <div class="item col-xs-4 col-lg-4" v-for="product in products">
+                    <div class="thumbnail">
+                        <img class="group list-group-image" :src="product.image" alt="@{{ product.title }}" />
+                        <div class="caption">
+                            <h4 class="group inner list-group-item-heading">@{{ product.title }}</h4>
+                            <p class="group inner list-group-item-text">@{{ product.description }}</p>
+                            <div class="row">
+                                <div class="col-xs-12 col-md-6">
+                                    <p class="lead">$@{{ product.price }}</p>
+                                </div>
+                                <div class="col-xs-12 col-md-6">
+                                    <a class="btn btn-success" href="#">Add to cart</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.26/vue.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/1.0.1/vue-resource.min.js"></script>
