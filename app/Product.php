894980e5e4a9d93bb1e48ca9fe6dd1b941f18a7b
Aung Khant <theaungkhant@gmail.com>
2016-11-06 21:43:41 +0630

    Configuring Algolia Search

--- app/Product.php
+++ app/Product.php
@@ -3,8 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Product extends Model
 {
-    //
+    use Searchable;
 }
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+//Route for search function
+Route::get('/search',[
+	'as' => 'api.search',
+	'uses' => 'Api\SearchController@search'
+]);	
